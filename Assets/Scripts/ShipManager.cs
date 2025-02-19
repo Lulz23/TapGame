889573f7251e29d06f0d93a578a467fce77e55f0
Lulz23 <bluesnakeuser@gmail.com>
2025-02-18 17:18:29 -0700

    Started script for game manager

--- Assets/Scripts/ShipManager.cs
+++ Assets/Scripts/ShipManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Clicker : MonoBehaviour
+public class ShipManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
